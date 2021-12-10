e4e4b42a75f3f93470977f6c0c382283b79249ef
ltf722 <70614069+ltf722@users.noreply.github.com>
2021-12-10 21:24:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git chinadns-ng https://github.com/pexcn' >>feeds.conf.default
-echo 'src-git smartdns https://github.com/pymumu/smartdns/tree/master/package/openwrt' >>feeds.conf.default
+#echo 'src-git smartdns https://github.com/pymumu/smartdns/tree/master/package/openwrt' >>feeds.conf.default
